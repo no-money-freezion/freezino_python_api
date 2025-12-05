aea75347ce0086e5301603f4d56ccaf3eb87f859
Ketarica <120320477+Ketarica@users.noreply.github.com>
2025-12-05 12:10:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -128,8 +128,6 @@ def login_user(user_data: UserLogin):
                 "refresh_token": "fake2",
             },
         }
-    except HTTPException:
-        raise
     except Exception as e:
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Внутренняя ошибка сервера")
